1d6f19460f62dff098bbe118b19016a29b534dab
RileyG00 <riley.griffin00@outlook.com>
2023-08-04 22:16:20 -0700

    Restarting

--- src/components/atoms/buttons/SubmitBtn.jsx
+++
@@ -1,9 +0,0 @@
-const SubmitBtn = ({classList, text}) => {
-    return (
-        <button type="submit" className={classList} title={text}>
-            {text}
-        </button>
-    )
-}
-
-export default SubmitBtn;
